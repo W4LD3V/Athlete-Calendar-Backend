9cd2ae1a0174f7aee83f03e85cb285bb99d84748
vendetta <vendetta@pc-2.local>
2024-02-21 16:09:19 +0200

    Switch to bcryptjs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const pgp = require("pg-promise")();
 
