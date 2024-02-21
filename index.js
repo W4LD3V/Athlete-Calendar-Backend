dd00c1b7c7d21241c837bb9f34b22739f96f4218
vendetta <vendetta@pc-2.local>
2024-02-21 16:18:36 +0200

    Added procfile

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
